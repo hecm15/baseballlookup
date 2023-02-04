df7fd5f412482e835656e1046377157561a30d48
Hector <hectormarmolejos615@gmail.com>
2023-02-04 11:22:48 -0500

    new database settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -82,11 +82,11 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 DATABASES = {
     'default' : {
         'ENGINE' : 'django.db.backends.postgresql',
-        'NAME' : 'baseballdatabank',
+        'NAME' : 'railway',
         'USER' : 'postgres',
-        'PASSWORD' : 'hm061500',
-        'HOST' : '127.0.0.1',
-        'PORT' : '5432'
+        'PASSWORD' : 'THCKyh2m0z4qbtasgYvJ',
+        'HOST' : 'containers-us-west-100.railway.app',
+        'PORT' : '6403'
     }
 }
 
